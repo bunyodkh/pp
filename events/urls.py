13e533aa8e76f7d052a8840b63c7287d550ab9d0
bunyodkh <bunyod.kh@gmail.com>
2025-05-28 09:24:41 +0500

    asd

--- events/urls.py
+++ events/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from .views import event_type_detail
 
 app_name = 'events'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/ru/#events', permanent=False), name='events-redirect'),
     path('<slug:slug>', event_type_detail, name='eventtype-detail'),
 ]
